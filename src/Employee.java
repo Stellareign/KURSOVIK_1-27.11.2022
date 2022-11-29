acfe54cbb136dc5d492a0e886d54d09b896c1c29
Stellareign <tuguran@mail.ru>
2022-11-29 22:39:23 +0300

    Продолжим. Когда не понимал, да ещё и забыл. :(

---
+++ src/Employee.java
@@ -0,0 +1,94 @@
+import java.util.Objects;
+// данные о сотрудниках
+public class  Employee {
+    private String employeeFirstName;
+    private String employeeSecondName;
+    private String employeeSurname;
+    private int department;
+    private String positionInDepartment;
+    private double salary;
+    private static int counter = 1; // статический счётчик. + понять, чтобы запомнить....
+    private final int id; // инициализировать перед конструктором, если внутри, то не выводится в toString
+    public Employee(String employeeSurname, String employeeFirstName, String employeeSecondName, int department,
+                    String positionInDepartment, double salary) { // если переменная final, то в конструктор по умолчанию попадает только она.
+        // id добавляем в return, данные вводятся вручную, id - автоматически
+        this.employeeFirstName = employeeFirstName;
+        this.employeeSecondName = employeeSecondName;
+        this.employeeSurname = employeeSurname;
+        this.department = department;
+        this.positionInDepartment = positionInDepartment;
+        this.salary = salary;
+
+        id = counter;
+        counter++;
+    }
+
+    @Override
+    public String toString() {
+        return "Табельный номер сотрудника: "+ id + " ФИО: " + employeeSurname + " " + employeeFirstName + " " + employeeSecondName +
+                ", отдел: " + department + ", " + positionInDepartment + ", оклад: " + salary +" руб."; // выводим ВСЕ данные о сотруднике
+    }
+
+    public String getEmployeeFirstName() { // генерим геттеры
+        return employeeFirstName;
+    }
+
+    public String getEmployeeSecondName() {
+        return employeeSecondName;
+    }
+
+    public String getEmployeeSurname() {
+        return employeeSurname;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public String getPositionInDepartment() {
+        return positionInDepartment;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public Employee setDepartment(int department) {
+        this.department = department;
+        return this;
+    }
+
+    public Employee setPositionInDepartment(String positionInDepartment) {
+        this.positionInDepartment = positionInDepartment;
+        return this;
+    }
+
+    public Employee setSalary(double salary) {
+        this.salary = salary;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id; // сравниваем по id
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id); // сравниваем хэш по id
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Курсовая работа №1");
+
+    Employee employee1 = new Employee("Илья", "Сергеевич", "Рогожкин", 1, "Технолог 1 категории", 55000);
+    Employee employee2 = new Employee("Анна", "Александровна", "Замятина", 1, "Заместитель начальника", 72700);
+    Employee employee3 = new Employee("Мария", "Владимировна", "Малинина", 1, "Начальник отдела", 90600);
+    Employee employee4 = new Employee("Андрей", "Александрович", "Тихонов", 4, "Начальник отдела", 91400);
+    Employee employee5 = new Employee("Алексей", "Николаевич", "Семёнов", 4, "Ведущий инженер", 65000);
+    Employee employee6 = new Employee("Владимир", "Арнольдович", "Никешин", 3, "Начальник отдела", 87800);
+    Employee employee7 = new Employee("Владислав", "Аркадиевич", "Загнибеда", 3, "Администратор", 45400);
+    Employee employee8 = new Employee("Елена", "Анатольевна", "Молчанова", 2, "Начальинк отдела", 92000);
+    Employee employee9 = new Employee("Людмила", "Дмитриевна", "Коваль", 2, "Ведущий технолог", 64230);
+    Employee employee10 = new Employee("Денис", "Ярославович", "Казанов", 5, "Водитель", 49600);
+        System.out.println(employee2);
+        System.out.println(employee1);
+        System.out.println(employee5);
     }
+
+
 }
